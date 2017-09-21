610fc88a487855dcfef2802e0db181e3c8745cc5
Chris Parnin <chris.parnin@gmail.com>
2017-09-21 15:18:18 -0400

    default call

--- main.js
+++ main.js
@@ -2,21 +2,50 @@ var fs = require('fs'),
     xml2js = require('xml2js'),
     child  = require('child_process'); 
 var parser = new xml2js.Parser();
+var Bluebird = require('bluebird')
 
 var testReport =  '/simplecalc/target/surefire-reports/TEST-com.github.stokito.unitTestExample.calculator.CalculatorTest.xml';
 
-try{
-    child.execSync('cd simplecalc; mvn test');
-}
-catch(e)
+calculatePriority();
+//findFlaky();
+
+async function findFlaky()
 {
-    //console.log(e);
+    for( var i = 0; i < 20; i++ )
+    {
+        try{
+            child.execSync('cd simplecalc; mvn test');
+        }catch(e){}
+        var contents = fs.readFileSync(__dirname + testReport)
+        let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
+        var tests = readResults(xml2json);
+        tests.forEach( e => console.log(i, e));
+    }
 }
-fs.readFile(__dirname + testReport, function(err, data) {
-    parser.parseString(data, function (err, result) {
-        console.dir(result);
-        // Print out everything
-        //console.dir(JSON.stringify(result,null, 3));
-        console.log('Done');
-    });
-});
+
+function readResults(result)
+{
+    var tests = [];
+    for( var i = 0; i < result.testsuite['$'].tests; i++ )
+    {
+        var testcase = result.testsuite.testcase[i];
+        
+        tests.push({
+        name:   testcase['$'].name, 
+        time:   testcase['$'].time, 
+        status: testcase.hasOwnProperty('failure') ? "failed": "passed"
+        });
+    }    
+    return tests;
+}
+
+async function calculatePriority()
+{
+    try{
+        child.execSync('cd simplecalc; mvn test');
+    }catch(e){}
+    var contents = fs.readFileSync(__dirname + testReport)
+    let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
+    var tests = readResults(xml2json);
+    tests.forEach( e => console.log(e));
+}
